1de238b886f17426795ed9787aac024a106c7480
laocecilia <clao4@binghamton.edu>
2015-10-10 17:10:01 -0700

    First commit- Login screen only

---
+++ BlackoutBen/app/src/main/java/com/example/blackoutben/ConfirmLogin.java
@@ -0,0 +1,35 @@
+package com.example.blackoutben;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class ConfirmLogin extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_login);
+        Intent intent = getIntent();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
