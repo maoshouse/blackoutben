5633fbb9736122e91639b9cdae1c69c6c4720497
Hsin-Mao Wu <hsinmao@maoshouse.com>
2015-10-11 00:58:52 -0700

    Add location updates

--- BlackoutBen/app/src/main/java/com/example/blackoutben/ConfirmLogin.java
+++ BlackoutBen/app/src/main/java/com/example/blackoutben/ConfirmLogin.java
@@ -1,21 +1,112 @@
 package com.example.blackoutben;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.ActionBarActivity;
+import android.location.Location;
+
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.maoshouse.myapplication.backend.locApi.model.Loc;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class ConfirmLogin extends ActionBarActivity {
+public class ConfirmLogin extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mCurrentLocation;
+    private TextView tv_userName;
+
+    protected Button mStartButton;
+    protected Button mStopButton;
+
+    protected Boolean mRequestingLocationUpdates;
+
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        createLocationRequest();
+    }
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        updateUI();
+    }
+
+    private void updateUI() {
+        if (mCurrentLocation != null) {
+            tv_userName.setText(String.valueOf(mCurrentLocation.getLatitude())+ " , "+ String.valueOf(mCurrentLocation.getLongitude()));
+            //mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    private void setButtonsEnabledState() {
+        if (mRequestingLocationUpdates) {
+            mStartButton.setEnabled(false);
+            mStopButton.setEnabled(true);
+        } else {
+            mStartButton.setEnabled(true);
+            mStopButton.setEnabled(false);
+        }
+    }
+
+    public void StartButtonHandler(View view) {
+        if (!mRequestingLocationUpdates) {
+            mRequestingLocationUpdates = true;
+            setButtonsEnabledState();
+            startLocationUpdates();
+        }
+    }
+
+    public void stopButtonHandler(View view) {
+        if (mRequestingLocationUpdates) {
+            mRequestingLocationUpdates = false;
+            setButtonsEnabledState();
+            stopLocationUpdates();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_login);
+
+        buildGoogleApiClient();
 
         SharedPreferences sharedPref = getSharedPreferences("user.xml", 0);
         String userName = sharedPref.getString(Constants.USER_NAME,"DEFAULT");
@@ -23,7 +114,7 @@ public class ConfirmLogin extends ActionBarActivity {
         String groupID = sharedPref.getString(Constants.GROUP_ID,"DEFAULT");
 
 
-        TextView tv_userName = (TextView)findViewById(R.id.userName);
+        tv_userName = (TextView)findViewById(R.id.userName);
         tv_userName.setText(userName);
         TextView tv_groupID = (TextView)findViewById(R.id.groupID);
         tv_groupID.setText(groupID);
@@ -31,8 +122,38 @@ public class ConfirmLogin extends ActionBarActivity {
         TextView tv_phoneNumber = (TextView)findViewById(R.id.phoneNumber);
         tv_phoneNumber.setText(phoneNumber);
 
+        mStartButton = (Button) findViewById(R.id.button_startLoc);
+        mStopButton = (Button) findViewById(R.id.button_stopLoc);
+        mRequestingLocationUpdates = false;
+        //startLocationUpdates();
 
 
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (mCurrentLocation == null) {
+            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            updateUI();
+        }
+
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    /**
+     * Google api callback methods
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
     }
 
     @Override
